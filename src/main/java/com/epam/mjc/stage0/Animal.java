f91db934485f543b571be98044848fee565bb567
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-07-09 16:31:24 +0400

    3rd_try

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -5,9 +5,6 @@ public class Animal {
     private int numberOfPaws;
     private boolean hasFur;
 
-    public Animal() {
-    }
-
     public Animal(String color, int numberOfPaws, boolean hasFur) {
         this.color = color;
         this.numberOfPaws = numberOfPaws;
@@ -15,13 +12,8 @@ public class Animal {
     }
 
     String getDescription() {
-        String fur = hasFur ? " a fur." : " no fur.";
-        String paws = numberOfPaws == 1 ? numberOfPaws + " paw " : numberOfPaws + " paws ";
-        return "This animal is mostly " + color + ". It has " + paws + "and" + fur;
-    }
-
-    public static void main(String[] args) {
-        Animal animal = new Animal("red", 1, true);
-        System.out.println(animal.getDescription());
+        String fur = this.hasFur ? " a " : " no ";
+        String paws = this.numberOfPaws == 1 ? " paw " : " paws ";
+        return "This animal is mostly " + this.color + ". It has " + this.numberOfPaws + paws + "and" + fur + "fur.";
     }
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -7,13 +7,6 @@ public class Bird extends Animal {
 
     @Override
     public String getDescription() {
-        super.getDescription();
         return ("This animal is mostly blue. It has 2 paws and no fur. Moreover, it has 2 wings and can fly.");
     }
-
-    public static void main(String[] args) {
-        Bird bird = new Bird();
-        System.out.println(bird.getDescription());
-    }
-
 }
